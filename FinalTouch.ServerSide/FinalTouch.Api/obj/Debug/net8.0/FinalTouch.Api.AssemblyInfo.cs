354be9eb18ada0e0ec3f3d150cb2414f0d5a1342
ziad501 <ziadmuhammed501@gmail.com>
2025-04-15 09:17:22 +0200

    SCRUM-12 fix(shop): resolve issue with filtering and pagination logic

--- FinalTouch.ServerSide/FinalTouch.Api/obj/Debug/net8.0/FinalTouch.Api.AssemblyInfo.cs
+++ FinalTouch.ServerSide/FinalTouch.Api/obj/Debug/net8.0/FinalTouch.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalTouch.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd5998632e13ea6f761d7271418c41fb7734a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9264cf671320d65820fe172362fd105ff9fd4c19")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalTouch.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalTouch.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

9c96ae483afa26455e7f74b95f576191cf2b8276
mizosaood32 <mizosaood23@gmail.com>
2025-04-15 04:17:57 +0200

    SCRUM-10 feat(api): add error handling middleware, validation responses, and CORS support

---
+++ FinalTouch.ServerSide/FinalTouch.Api/Controllers/BuggyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Claims;
+using FinalTouch.Api.Controllers;
+using FinalTouch.Api.Dtos;
+using FinalTouch.Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("badrequest")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("Not a good request");
+    }
+
+    [HttpGet("notfound")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("internalerror")]
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("This is a test exception");
+    }
+
+    [HttpPost("validationerror")]
+    public IActionResult GetValidationError(CreateProductDto product)
+    {
+        return Ok();
+    }
+
+
+
+
+}

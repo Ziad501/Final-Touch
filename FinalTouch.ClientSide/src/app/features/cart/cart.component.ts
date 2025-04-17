6acdbd963b256fc276ddccc9d3227d8ece62a1d7
mohamedmohsen211 <mohamed.mohsen.21199@gmail.com>
2025-04-17 21:53:59 +0200

    SCRUM-17 feat(auth): implement login/register forms, auth guard, interceptors, and persistent user state, add stripe.js to the project

--- FinalTouch.ClientSide/src/app/features/cart/cart.component.ts
+++ FinalTouch.ClientSide/src/app/features/cart/cart.component.ts
@@ -2,12 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CartService } from '../../core/services/cart.service';
 import { CartItemComponent } from "./cart-item/cart-item.component";
 import { OrderSummaryComponent } from "../../shared/component/order-summary/order-summary.component";
-import { RouterLink } from '@angular/router';
-import { MatButton } from '@angular/material/button';
+import { EmptyStateComponent } from '../../shared/component/empty-state/empty-state.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItemComponent, OrderSummaryComponent,RouterLink,MatButton],
+  imports: [CartItemComponent, OrderSummaryComponent,EmptyStateComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })

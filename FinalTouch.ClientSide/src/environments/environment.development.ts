99fcba34933d2bd530ca42b2f6283a52ca54320d
mohamedmohsen211 <mohamed.mohsen.21199@gmail.com>
2025-04-17 22:28:31 +0200

    SCRUM-19 feat(checkout): implement layout, address form, Update cart componeent confiact and update the Environment and add cart.ts type and checkout component

--- FinalTouch.ClientSide/src/environments/environment.development.ts
+++ FinalTouch.ClientSide/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/api/'
+  apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey: 'pk_test_51REqo54ZpnPWuFDBSqajibextnDdcSySOSO5f8GCkpgHg6BSB3az25OvM5BwJiP8rPEDC8MhcGFSeA5JVSscTcXx00Cj2vwppF'
 };
--- FinalTouch.ClientSide/src/environments/environment.ts
+++ FinalTouch.ClientSide/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'api/'
+  apiUrl: 'api/',
+  stripePublicKey: 'pk_test_51REqo54ZpnPWuFDBSqajibextnDdcSySOSO5f8GCkpgHg6BSB3az25OvM5BwJiP8rPEDC8MhcGFSeA5JVSscTcXx00Cj2vwppF'
+
 };

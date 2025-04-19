767913135c985e1de10b14ef606e6ceec6bb9754
ziad501 <ziadmuhammed501@gmail.com>
2025-04-19 17:55:36 +0200

    add stripe key to evironment components

--- FinalTouch.ClientSide/src/environments/environment.ts
+++ FinalTouch.ClientSide/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'api/',
   hubUrl: 'hub/notifications',
-  stripePublicKey: 'pk_test_51REqo54ZpnPWuFDBSqajibextnDdcSySOSO5f8GCkpgHg6BSB3az25OvM5BwJiP8rPEDC8MhcGFSeA5JVSscTcXx00Cj2vwppF'
+  stripePublicKey: 'pk_test_51RFcMBQoxCrcmYQlkePdXomcLQQXKxjZnobks6cLX1rwFJPx6iKw1RqxGos3poMA86ljgQvIQQypZorgaMND9fVI00RFjwuTLU'
 
 };
